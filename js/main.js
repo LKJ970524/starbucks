391278a26aed95e20e3a518779045ffefffb7f2e
LKJ970524 <dlrbwjd7096@naver.com>
2024-02-06 22:09:34 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges')
 const toTopEl = document.querySelector('#to-top')
+
 // 페이지에 스크롤 이벤트를 추가!
 window.addEventListener('scroll', _.throttle(function () {
   // 페이지 스크롤 위치가 500px이 넘으면.
@@ -156,8 +139,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-// 올해가 몇년도인지 계산
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
